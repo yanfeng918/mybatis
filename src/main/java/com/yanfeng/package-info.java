bcba8ff552cd954ddb19843072227aff064b164d
yanfeng <yanfeng>
2016-05-19 14:11:02 +0800

    Initial commit

---
+++ src/main/java/com/yanfeng/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Created by yanfeng on 2016/5/19.
+ */
+package com.yanfeng;
---
+++ src/test/java/com/yanfeng/UserTest.java
@@ -0,0 +1,51 @@
+package com.yanfeng;
+
+import com.yanfeng.dao.StudentMapper;
+import com.yanfeng.dao.UserMapper;
+import com.yanfeng.entity.Student;
+import com.yanfeng.entity.StudentExample;
+import com.yanfeng.entity.User;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
+
+/**
+ * Created by yanfeng on 2016/5/19.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations="classpath:applicationContext.xml")
+public class UserTest {
+//    @Autowired(required = true)
+//    @Qualifier("userMapper")
+////    @Resource
+//    private UserMapper userMapper;
+
+    @Autowired
+    private StudentMapper studentMapper;
+
+    @Test
+    public void getNearbyNum()  {
+        System.out.println(123);
+//        User load = userMapper.load(1);
+//        System.out.print(load.toString());
+    }
+
+    @Test
+    public void addStudent()  {
+        Student s = new Student();
+        s.setName("yanfeng");
+        studentMapper.insert(s);
+    }
+
+    @Test
+    public void test1(){
+        StudentExample example = new StudentExample();
+        example.or().andNameLike("%yan%");
+        System.out.print(studentMapper.selectByExample(example).get(0).toString());
+    }
+}
